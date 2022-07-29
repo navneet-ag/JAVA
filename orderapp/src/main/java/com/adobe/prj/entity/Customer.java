e2c7f5b2aaa5c4545015a34ff98189efb2c648d4
Banuprakash <banuprakashc@yahoo.co.in>
2022-07-29 15:06:20 +0530

    day 4

---
+++ orderapp/src/main/java/com/adobe/prj/entity/Customer.java
@@ -0,0 +1,45 @@
+package com.adobe.prj.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="customers")
+public class Customer {
+	@Id
+	private String email;
+	
+	@Column(name="first_name", length = 50, nullable = false)
+	private String firstName;
+	
+	@Column(name="last_name", length = 50)
+	private String lastName;
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	
+}
